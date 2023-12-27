4fc6d0890ec32493e9bf01599723f2bb67222c16
Matsvei Niaverau <matvej2002@gmail.com>
2023-12-27 10:03:13 +0100

    mt6768-common: Bring back Mediatek Power HAL
    Change-Id: I7bb964e089f1549f849daf08532baaeabcc99379

--- mt6768.mk
+++ mt6768.mk
@@ -248,25 +248,14 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.mediatek-libperfmgr
-
-PRODUCT_PACKAGES += \
-    libmtkperf_client_vendor \
-    libmtkperf_client
+    android.hardware.power-service-mediatek \
+    android.hardware.power-V2-ndk_platform.vendor \
+    android.hardware.power@1.3.vendor
 
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.0.vendor \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0.vendor \
-    android.hardware.power@1.1.vendor \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power@1.3.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
