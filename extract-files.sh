2c3c80430bfd729e07458ec15a6e64c89a83cc8f
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2026-02-16 09:36:55 +0000

    mt6768-common: link mtk sensor multihal with libbase_shim
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.sensors@2.0-service.multihal-mediatek": cannot locate symbol "_ZN7android4base15WriteStringToFdERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEENS0_11borrowed_fdE" referenced by "/vendor/bin/hw/android.hardware.sensors@2.0-service.multihal-mediatek"...

--- extract-files.sh
+++ extract-files.sh
@@ -116,7 +116,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
             ;;
-        vendor/lib/libnvram.so | vendor/lib/libsysenv.so | vendor/lib64/libnvram.so | vendor/lib64/libsysenv.so | vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron)
+        vendor/lib/libnvram.so | vendor/lib/libsysenv.so | vendor/lib64/libnvram.so | vendor/lib64/libsysenv.so | vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron | vendor/bin/hw/android.hardware.sensors@2.0-service.multihal-mediatek)
        [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
             ;;
