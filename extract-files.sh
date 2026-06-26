a27601b3cbea5b8c28add1754ac484c00c68f3f6
EmreDemirn <emre2021demir@hotmail.com>
2026-06-26 21:01:22 +0000

    mt6768-common: patch vendor.mediatek.hardware.pq@2.2-service

--- extract-files.sh
+++ extract-files.sh
@@ -72,7 +72,8 @@ function blob_fixup() {
             ;;
         vendor/lib/hw/vendor.mediatek.hardware.pq@2.13-impl.so|\
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so|\
-        vendor/lib*/hw/android.hardware.thermal@2.0-impl.so)
+        vendor/lib*/hw/android.hardware.thermal@2.0-impl.so|\
+	vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
